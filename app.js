184528bd02ac483536bed0d5dfa94a9f206ca561
Kaan Caliskan <mail@kcaliskan.com>
2019-04-03 10:58:35 -0700

    deleting data

--- app.js
+++ app.js
@@ -6,15 +6,26 @@ function renderCafe(doc) {
   let li = document.createElement("li");
   let name = document.createElement("span");
   let city = document.createElement("span");
+  let cross = document.createElement("div");
 
-  li.setAttribute("date-id", doc.id);
+  li.setAttribute("data-id", doc.id);
   name.textContent = doc.data().name;
   city.textContent = doc.data().city;
+  cross.textContent = "X";
 
   li.appendChild(name);
   li.appendChild(city);
+  li.appendChild(cross);
 
   cafeList.appendChild(li);
+
+  //deleting data
+  cross.addEventListener("click", e => {
+    let docId = e.target.parentElement.getAttribute("data-id");
+    db.collection("cafes")
+      .doc(docId)
+      .delete();
+  });
 }
 
 //gettin data
